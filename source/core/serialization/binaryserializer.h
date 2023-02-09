be1fbead5f9e656f84756df179ddf701ca66d448
krishpranav <krisna.pranav@gmail.com>
2023-02-09 20:15:46 +0530

    serialization: binaryserializer + serializer

--- source/core/serialization/binaryserializer.h
+++ source/core/serialization/binaryserializer.h
@@ -11,12 +11,25 @@
 
 #pragma once
 
-#include "serializer.h"
+#include "Serializer.h"
 
 class BinarySerializer final : public Serializer {
 public:
+
+    /**
+     * @brief save to file 
+     * 
+     * @param file 
+     * @param tree 
+     * @return Result 
+     */
     Result saveToFile(File file, const SerializedData &tree) const override;
+    SerializedData loadFromFile(const File &file) const override;
+
+    Result saveToString(String &string, const SerializedData &tree) const override;
+    SerializedData loadFromString(const String &string) const override;
 
     bool supportsFileWithExtension(const String &extension) const override;
     bool supportsFileWithHeader(const String &header) const override;
+
 };
--- source/core/serialization/serializer.h
+++ source/core/serialization/serializer.h
@@ -14,7 +14,33 @@
 class Serializable;
 class SerializedData;
 
-class Serializer
-{
+class Serializer {
+public:
+
+    virtual ~Serializer() = default;
+
+    /**
+     * @brief Save to file, save to string
+     * 
+     * @param file 
+     * @param tree 
+     * @return Result 
+     */
+    virtual Result saveToFile(File file, const SerializedData &tree) const = 0;
+    virtual SerializedData loadFromFile(const File &file) const = 0;
+
+    virtual Result saveToString(String &string, const SerializedData &tree) const = 0;
+    virtual SerializedData loadFromString(const String &string) const = 0;
+
+
+    /**
+     * @brief Support file with extension or header
+     * 
+     * @param extension 
+     * @return true 
+     * @return false 
+     */
+    virtual bool supportsFileWithExtension(const String &extension) const = 0;
+    virtual bool supportsFileWithHeader(const String &header) const = 0;
 
 };
