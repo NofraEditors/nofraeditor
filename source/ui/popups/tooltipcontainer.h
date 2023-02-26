47a343989bde8f3b297071b75d6221a27c7e3179
krishpranav <krisna.pranav@gmail.com>
2023-02-26 17:47:46 +0530

    ui: tooltips

--- source/ui/popups/tooltipcontainer.h
+++ source/ui/popups/tooltipcontainer.h
@@ -11,18 +11,44 @@
 
 #pragma once
 
+/**
+ * @brief Tool Tip Container
+ * 
+ */
 class TooltipContainer final : public Component, private Timer {
 public:
+
     TooltipContainer();
     ~TooltipContainer();
 
+    void showWithComponent(UniquePointer<Component> targetComponent,
+        int timeOutMs = -1);
+
+    void showWithComponent(UniquePointer<Component> targetComponent,
+        Rectangle<int> callerScreenBounds, int timeOutMs = -1);
+
+    void hide();
+
+    void paint(Graphics &g) override;
+    void resized() override;
+    void parentHierarchyChanged() override;
+    void parentSizeChanged() override;
+
 private:
+
     int hideTimeout = -1;
-    int timeCounter = -1; 
+    int timeCounter = -1;
     bool alignedToBottom = true;
+    int clicksCountOnStart = 0;
+
+    ComponentAnimator animator;
 
     void timerCallback() override;
     void updatePosition();
 
+    UniquePointer<Component> tooltipComponent;
+
     static constexpr auto timerMs = 100;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TooltipContainer)
 };
